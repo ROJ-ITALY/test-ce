38c3d6dc28709d1c1336b5c640db236ecdf99406
Marco Arlone <marco.arlone@roj.com>
2018-03-06 11:53:18 +0100

    - Upgrade 'common.py', 'scheduler.json' and 'config.json'. - Delete 'test_display.py'. - New test 'test_usb.py'.

--- scheduler.py
+++ scheduler.py
@@ -3,25 +3,77 @@
 import os
 import sys
 import subprocess
+import time
 import argparse
+import tempfile
+
+test_list = ['usb']
 
 ###############################################################################
-#	Scheduler_error
+#	class Scheduler
+###############################################################################
+class Scheduler:
+	def __init__(self, tests, count, nostop):
+		self.tests = tests
+		self.count = count
+		self.nostop = nostop
+		self.test_failed = dict()
+
+	def start_test(self, i, n, name):
+		if (name == 'all'):
+			for t in test_list:
+				ret = self.start_test(i, n, t)
+		else:
+			print('------------------------------')
+			print(' %d/%d - %s' % (i, n, name))
+			print('------------------------------')
+			ret = subprocess.run(['python3', 'test_%s.py' % name])
+			if ret.returncode != 0:
+				if name in self.test_failed:
+					self.test_failed[name] += 1
+				else:
+					self.test_failed[name] = 1
+				if not args.nostop:
+					raise Scheduler_error(2)
+
+		return ret.returncode
+
+	def start(self):
+		if self.tests == None:
+			tests = test_list
+		else:
+			tests = self.tests
+
+		full_test = True
+		if 'all' not in self.tests:
+			for t in test_list:
+				if t not in self.tests:
+					full_test = False
+					break
+
+		inf_path = tempfile.gettempdir() + '/inf.txt'
+		if os.path.exists(inf_path):
+			os.remove(inf_path)
+
+		for i in range(0, self.count):
+			for t in self.tests:
+				self.start_test(i+1, self.count, t)
+			self.show_report(i+1)
+
+	def show_report(self, i):
+		print('------------------------------')
+		print('   Report %d' % i)
+		print('------------------------------')
+		print('Tests failed: %s' % scheduler.test_failed)
+
+###############################################################################
+#	class Scheduler_error
 ###############################################################################
 class Scheduler_error(Exception):
-	def __init__(self, msg):
-		self.msg = msg
-		
-###############################################################################
-def start(i, n, name):
-	print('--------------------')
-	print(' %s/%s  %s' % (i, n, name))
-	print('--------------------')
-	if name == 'calibrate':
-		ret = subprocess.call(['/home/vdw/customer_privileged_scripts/calibrate.sh'])
-	return ret;
+	def __init__(self, code):
+		self.code = code
 
-test_list = ['calibrate']
+###############################################################################
 arg_test_list = list(test_list)
 arg_test_list.append('all')
 
@@ -31,33 +83,17 @@ parser.add_argument('--nostop', action="store_true", help="continue on error")
 parser.add_argument('tests', type=str, choices=arg_test_list, nargs='*', help='tests list')
 args = parser.parse_args()
 
-if os.getuid() != 0:
-	print('Error: non root')
-	sys.exit(-1)
-
-if args.tests == None:
-	tests = test_list
-else:
-	tests = args.tests
-
-error_found = False
 try:
-	n = args.count
-	for i in range(0, n):
-		for t in tests:
-			if t == 'all':
-				for t1 in test_list:
-					if start(i+1, n, t1) != 0:
-						error_found = True
-						if not args.nostop:
-							raise Scheduler_error(t1)
-			else:
-				if start(i+1, n, t) != 0:
-					error_found = True
-					if not args.nostop:
-						raise Scheduler_error(t)
+	if os.getuid() != 0:
+		raise Scheduler_error(1)
+
+	scheduler = Scheduler(args.tests, args.count, args.nostop)
+	scheduler.start()
 
 except Scheduler_error as e:
-	sys.exit(-1)
-
-sys.exit(0)
+	if e.code == 1:
+		s = "Non root"
+	elif e.code == 2:
+		s = "Test failed"
+	print('scheduler.py: error: %s' % s)
+	sys.exit(e.code)
---
+++ test_usb.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import time
+import argparse
+from common import *
+
+###############################################################################
+#	class Test_usb
+###############################################################################
+class Test_port(Test_basic):
+	def __init__(self):
+		Test_basic.__init__(self, 'usb')
+		self.err_dict['ENABLE1_TIMEOUT'] = 'USB 1 enable timeout'
+		self.err_dict['ENABLE2_TIMEOUT'] = 'USB 2 enable timeout'
+		self.err_dict['DISABLE1_TIMEOUT'] = 'USB 1 disable timeout'
+		self.err_dict['DISABLE2_TIMEOUT'] = 'USB 2 disable timeout'
+		self.err_dict['CHECK1_FAILED'] = 'Check USB 1 failed or invalid USB key 1'
+		self.err_dict['CHECK2_FAILED'] = 'Check USB 2 failed or invalid USB key 2'
+		self.err_dict['MOUNT1_FAILED'] = 'Mount USB 1 failed'
+		self.err_dict['MOUNT2_FAILED'] = 'Mount USB 2 failed'
+	
+	def initialize(self):
+		Test_basic.initialize(self)
+
+	def finalize(self):
+		try:
+			Test_basic.finalize(self)
+		except Test_error as e:		
+			sys.exit(-1)
+
+	def check(self):
+		ret = subprocess.run(['findmnt', '-S', 'LABEL=\"%s\"' % self.label1, '-n', '-o', 'TARGET'], stdout=subprocess.PIPE)
+		path1 = ret.stdout.decode('utf-8')[:-1]
+		if not os.path.exists(path1):
+			raise Test_error(self, 'MOUNT1_FAILED')
+		if subprocess.run(['sha256sum', '-c', 'test-file.sha256', '--quiet'], stdout=subprocess.DEVNULL, cwd=path1).returncode != 0:
+			raise Test_error(self, 'CHECK1_FAILED')
+
+		ret = subprocess.run(['findmnt', '-S', 'LABEL=\"%s\"' % self.label2, '-n', '-o', 'TARGET'], stdout=subprocess.PIPE)
+		path2 = ret.stdout.decode('utf-8')[:-1]
+		if not os.path.exists(path2):
+			raise Test_error(self, 'MOUNT2_FAILED')
+		if subprocess.run(['sha256sum', '-c', 'test-file.sha256', '--quiet'], stdout=subprocess.DEVNULL, cwd=path2).returncode != 0:
+			raise Test_error(self, 'CHECK2_FAILED')
+
+###############################################################################
+try:
+	t = Test_usb()
+	
+	parser = argparse.ArgumentParser(description='Test USB')
+	t.add_common_arguments(parser)
+	parser.add_argument('--label1', type=str, default=t.config['usb']['label1'], help="set pen drive 1 label")
+	parser.add_argument('--label2', type=str, default=t.config['usb']['label2'], help="set pen drive 2 label")
+	args = parser.parse_args()
+	t.copy_common_arguments(args)
+	t.label1 = args.label1
+	t.label2 = args.label2
+
+	t.initialize()
+
+	t.check()
+
+	t.success()
+
+except Test_error as e:
+	e.test.error(e.code, e.value)
+
+
