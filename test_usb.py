49cea1679db05de715790c0e4e0a5d5aeb83e784
Docker Administrator <dockeradministrator@VDWMARALJK01.vandewiele.local>
2018-10-17 10:58:45 +0200

    Update test usb

--- test_usb.py
+++ test_usb.py
@@ -12,10 +12,9 @@ from common import *
 class Test_usb(Test_basic):
 	def __init__(self):
 		Test_basic.__init__(self, 'usb')
+		self.err_dict['LABEL_A_FAILED'] = 'Check pen drive A failed or label A not found'
 		self.err_dict['CHECK_A_FAILED'] = 'Check pen drive A failed or invalid pen drive A'
-		self.err_dict['CHECK_B_FAILED'] = 'Check pen drive B failed or invalid pen drive B'
 		self.err_dict['MOUNT_A_FAILED'] = 'Mount pen drive A failed'
-		self.err_dict['MOUNT_B_FAILED'] = 'Mount pen drive B failed'
 	
 	def initialize(self):
 		Test_basic.initialize(self)
@@ -23,25 +22,24 @@ class Test_usb(Test_basic):
 	def finalize(self):
 		try:
 			Test_basic.finalize(self)
-		except Test_error as e:		
+		except Test_error as e:
 			sys.exit(-1)
 
 	def check(self):
 		self.message('Check USB key A')
-		ret = subprocess.run(['findmnt', '-S', 'LABEL=\"%s\"' % self.label_a, '-n', '-o', 'TARGET'], stdout=subprocess.PIPE)
-		path_a = ret.stdout.decode('utf-8')[:-1]
-		if not os.path.exists(path_a):
+		mnt_path = '/mnt/{}'.format(self.label_a)
+		dev = '/dev/disk/by-label/{}'.format(self.label_a)
+		if not os.path.isdir(mnt_path):
+			os.mkdir(mnt_path)
+		if not os.path.isdir(dev):
+			raise Test_error(self, 'LABEL_A_FAILED')
+		if subprocess.run(['mount', dev, mnt_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).returncode != 0:
+			subprocess.run(['umount', mnt_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 			raise Test_error(self, 'MOUNT_A_FAILED')
-		if subprocess.run(['sha256sum', '-c', 'test-file.sha256'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=path_a).returncode != 0:
+		if subprocess.run(['sha256sum', '-c', 'test-file.sha256'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=mnt_path).returncode != 0:
+			subprocess.run(['umount', mnt_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 			raise Test_error(self, 'CHECK_A_FAILED')
-
-		self.message('Check USB key B')
-		ret = subprocess.run(['findmnt', '-S', 'LABEL=\"%s\"' % self.label_b, '-n', '-o', 'TARGET'], stdout=subprocess.PIPE)
-		path_b = ret.stdout.decode('utf-8')[:-1]
-		if not os.path.exists(path_b):
-			raise Test_error(self, 'MOUNT_B_FAILED')
-		if subprocess.run(['sha256sum', '-c', 'test-file.sha256'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=path_b).returncode != 0:
-			raise Test_error(self, 'CHECK_B_FAILED')
+		subprocess.run(['umount', mnt_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 ###############################################################################
 try:
@@ -50,11 +48,9 @@ try:
 	parser = argparse.ArgumentParser(description='Test USB')
 	t.add_common_arguments(parser)
 	parser.add_argument('--labela', type=str, default=t.config['usb']['labela'], help="set pen drive A label")
-	parser.add_argument('--labelb', type=str, default=t.config['usb']['labelb'], help="set pen drive B label")
 	args = parser.parse_args()
 	t.copy_common_arguments(args)
 	t.label_a = args.labela
-	t.label_b = args.labelb
 
 	t.initialize()
 
